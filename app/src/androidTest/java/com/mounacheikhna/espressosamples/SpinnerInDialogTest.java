7348a7c4dc55c673318d0024c279316065e1c9b3
Mouna Cheikhna <cheikhnamouna@gmail.com>
2015-12-27 14:56:55 +0100

    A working solution for spinner selection with espresso.

--- app/src/androidTest/java/com/mounacheikhna/espressosamples/SpinnerInDialogTest.java
+++ app/src/androidTest/java/com/mounacheikhna/espressosamples/SpinnerInDialogTest.java
@@ -2,6 +2,12 @@ package com.mounacheikhna.espressosamples;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.AllOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,13 +15,11 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by cheikhnamouna on 12/27/15.
@@ -30,24 +34,32 @@ public class SpinnerInDialogTest {
     onView(withId(R.id.show_dialog)).perform(click());
 
     onView(withId(R.id.planets_spinner))
-        //.inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
-        .inRoot(isDialog())
         .perform(click());
 
-    onData(allOf(is(instanceOf(String.class)), is("Saturn")))
-        //.inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
-        .inRoot(isDialog())
+    onData(AllOf.allOf(is(instanceOf(String.class)), is("Saturn")))
+        .inRoot(Matchers.allOf(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))),
+            withDecorView(hasWindowFocus())))
         .perform(click());
 
     onView(withId(R.id.ok_dialog))
-        //.inRoot(withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView()))))
-        .inRoot(isDialog())
         .perform(click());
 
-    onView(withId(R.id.planets_spinner))
-        .inRoot(isDialog())
-        .perform(click());
-    onView(withId(R.id.planets_spinner)).check(matches(withSpinnerText("Saturn")));
+    onView(withId(R.id.show_dialog)).perform(click());
   }
+
+  private static Matcher<View> hasWindowFocus() {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("has window focus");
+      }
+
+      @Override
+      public boolean matchesSafely(View view) {
+        return view.hasWindowFocus();
+      }
+    };
+  }
+
 
 }
